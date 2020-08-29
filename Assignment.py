4255a38145686542494f79a378be0b14458c09c5
D-e-v-i-k-a <70093823+D-e-v-i-k-a@users.noreply.github.com>
2020-08-29 20:06:49 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,39 @@
+2
+print("Name: DEVIKA PRASAD")
+print("Address:Melathil house,Thiruvallom PO, Trivandrum")
+print("telephone: 7559891569")
+
+3
+name = "devika prasad"
+print("Your name is",name)
+
+
+4
+width = int(input("Enter the width: "))
+height = int(input("Enter the height: "))
+area = width*height
+print("The area is ",area,"square units")
+
+5
+base = int(input("enter the base: "))
+height = int(input("enter the height: "))
+area = 0.5*base*height
+print("The area is",area,"square units")
+
+6
+radius = int(input("enter the radius: "))
+area = 3.14*radius**2
+print("The area of circle is",area,"square units")
+
+7
+name = input("enter the name: ")
+age = int(input("enter the age: "))
+print("Name is",name,"and age is",age)
+
+8
+num = int(input("enter a number: "))
+num = num+1
+print("Number is",num)
+
+9
+name = input("enter your first name")
